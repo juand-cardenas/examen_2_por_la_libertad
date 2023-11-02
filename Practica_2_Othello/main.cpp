3561d3dba8590c1a5d3ea5833d935240c118c8c8
Samuel Di Nicola <samueldne04@gmail.com>
2023-11-01 19:59:07 -0500

    Progreso en la cosntruccion del main

--- Practica_2_Othello/main.cpp
+++ Practica_2_Othello/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-#include <othello.h>
+#include <ctime>
+#include <sstream>
+#include "othello.h"
+
+using namespace std;
 
 int main()
-{
-    othello hola(8);
-    hola.imprimir_tablero();
+{   int pos_x,pos_y;
+    bool c = true;
+    othello game(8);
+
+    //Funcione ingresar nombres de los jugadores
+    game.imprimir_tablero();
+
+    while (c)
+    {
+      game.recibe_ficha_x();
+      game.recibe_ficha_y();
+    }
+      /*
+      game.P_Posibilidad_jugada();//if
+      //game.P_Validar_pos(pos_x,pos_y);
+      game.actualizar_tablero('*');
+      game.voltear_fichas('*', '-');
+      */
+
+
+
+
+
+
     return 0;
 }
--- Practica_2_Othello/othello.h
+++ Practica_2_Othello/othello.h
@@ -1,23 +1,45 @@
+using namespace std;
 #include <iostream>
+#include <iomanip>
 #ifndef OTHELLO_H
 #define OTHELLO_H
-
-using namespace std;
+#include "othello.h"
+#include <ctime>
+#include <sstream>
 
 class othello
 {
-public:
-
-othello(short int tamaño);
-void imprimir_tablero();
-
-
 private:
 
-char** tablero;
-int tamaño_tablero;
+    char** tablero;
+    int tamaño_tablero;
+    int posicion;
+    int pos_x,pos_y;
+    bool verificacion=true;
+    void imprimir_error_en_posicion();
+    void imprimir_fut_pos_x();
+    void verificar_2(int y);
+    void verificar(int x);
+    void imprimir_fut_pos_y();
+    bool Validar_pos(int pos_x,int pos_y);
+    bool Posibilidad_jugada();
 
 
+public:
+
+    void tiempo();
+    bool posicion_valida;
+    othello(int tamaño);
+    void imprimir_tablero();
+    void recibe_ficha_x();
+    void recibe_ficha_y();
+    void actualizar_tablero(char ficha);
+    void voltear_fichas(char ficha_alidad, char ficha_enemiga);
+    bool P_Posibilidad_jugada();
+    bool P_Validar_pos(int pos_x,int pos_y);
+
+
+    //bool terminarjuego(bool Val_pos,int &Cant_fB,int &Cant_fN,int tamaño);
 
 };
 
